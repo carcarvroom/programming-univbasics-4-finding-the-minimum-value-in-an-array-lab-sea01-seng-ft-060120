5d1aed3dcb5f6959192a92d4e7235ad4c93c2963
Carly L <carlylynnla@gmail.com>
2020-05-03 05:39:11 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,9 +1,9 @@
 def find_min_value(array)
   count = 0
-  min_value = -1
+  min_value = 100
   
   while count < array.length do 
-    if array[-count] < min_value
+    if array[count] < min_value
       min_value = array[count]
     end
     count += 1
